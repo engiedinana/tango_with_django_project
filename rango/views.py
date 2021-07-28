dc1203f75290ffec9457f376124a790f53256dc0
Engie Dinana <2517261D@student.gla.ac.uk>
2021-07-28 23:30:19 +0100

    client-side cookies done

--- rango/views.py
+++ rango/views.py
@@ -9,7 +9,27 @@ from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
 
+def visitor_cookie_handler(request, response):
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, then the default value of 1 is used.
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+    '%Y-%m-%d %H:%M:%S')
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        # Set the last visit cookie
+        response.set_cookie('last_visit', last_visit_cookie)
+    # Update/set the visits cookie
+    response.set_cookie('visits', visits)
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
@@ -23,8 +43,11 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
+    context_dict['visits'] = int(request.COOKIES.get('visits', '1'))
+    response = render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request, response)
     # Render the response and send it back!
-    return render(request, 'rango/index.html', context=context_dict)
+    return response
 
 def about(request):
     # Return a rendered response to send to the client.
